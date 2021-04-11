e1fce5ef88141f82fb3e51eb3ffb4cabc17a0aaa
Kaique Lopes <41844709+Kaique-Lopes@users.noreply.github.com>
2021-04-10 23:14:44 -0300

    showNext

--- App_Converter/App_Converter/ViewController.swift
+++ App_Converter/App_Converter/ViewController.swift
@@ -13,14 +13,39 @@ class ViewController: UIViewController {
     @IBOutlet weak var btUnit2: UIButton!
     @IBOutlet weak var lbResult: UILabel!
     @IBOutlet weak var lbResultUnit: UILabel!
+    @IBOutlet weak var lbUnit: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
     @IBAction func showNext(_ sender: UIButton) {
+        switch lbUnit.text {
+        case "Temperatura":
+            lbUnit.text = "Peso"
+            btUnit1.setTitle("Kilograma", for: .normal)
+            btUnit2.setTitle("Libra", for: .normal)
+        case "Peso":
+            lbUnit.text = "Moeda"
+            btUnit1.setTitle("Real", for: .normal)
+            btUnit2.setTitle("Dólar", for: .normal)
+
+        case "Moeda":
+            lbUnit.text = "Distancia"
+            btUnit1.setTitle("Metro", for: .normal)
+            btUnit2.setTitle("Quilometro", for: .normal)
+
+        default :
+            lbUnit.text = "Temperatura"
+            btUnit1.setTitle("Celsius", for: .normal)
+            btUnit2.setTitle("Farenheint", for: .normal)
+
+        }
     }
+    
     @IBAction func convert(_ sender: Any) {
+        
     }
     
 
